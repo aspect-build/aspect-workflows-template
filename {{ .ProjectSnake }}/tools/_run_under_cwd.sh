39aa87eb04d679b86c7b8740cb33800a3c4982f5
Alex Eagle <alex@aspect.dev>
2024-09-22 13:19:04 -0700

    rename to copier since we'll have other codegen in other langs

--- {{ .ProjectSnake }}/tools/_run_under_cwd.sh
+++ {{ .ProjectSnake }}/tools/_run_under_cwd.sh
@@ -4,7 +4,7 @@ case "$(basename "$0")" in
   buildifier)
     target="@buildifier_prebuilt//:buildifier"
     ;;
-{{- if .Computed.python }}
+{{- if .Computed.copier }}
   copier)
     target="//tools:copier"
     ;;
