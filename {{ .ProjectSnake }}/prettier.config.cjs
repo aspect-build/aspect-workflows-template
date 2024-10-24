b81de06750ea8fbfdd3699f39591d79b7f950a96
Alex Eagle <alex@aspect.dev>
2024-10-24 09:48:46 -0700

    Update prettier.config.cjs

--- {{ .ProjectSnake }}/prettier.config.cjs
+++ {{ .ProjectSnake }}/prettier.config.cjs
@@ -2,10 +2,7 @@
  * @see https://prettier.io/docs/en/configuration.html
  */
 const config = {
-  trailingComma: 'es5',
   tabWidth: 2,
-  semi: false,
-  singleQuote: true,
   plugins: [
     // Add plugins here using a require() statement.
     // This is required to ensure the node module resolution algorithm starts walking
